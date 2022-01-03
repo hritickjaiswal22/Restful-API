3abf3e651879376599abbd7007ca5aa37e253c08
Hritick Jaiswal <newbiehritick@gmail.com>
2022-01-03 20:54:45 +0530

    Connected express api to Local mongodb using mongoose

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const url = "mongodb://127.0.0.1:27017/testAPI";
 
 const app = express();
 
-//* Middlewares
-app.use("/posts", () => {
-  console.log(
-    "When /posts route is hit, this callback function is executed first"
-  );
-});
+(async () => {
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${url}`);
+  } catch (err) {
+    console.error(err);
+  }
+})();
 
 app.get("/", (req, res) => {
   res.send("In home");
