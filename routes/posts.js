f72b7e65878a64230378e698a70052e40108ac70
Hritick Jaiswal <newbiehritick@gmail.com>
2022-01-04 17:30:20 +0530

    Learned how to save data to mongodb database

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
 
 router.get("/", (req, res) => {
   res.send("GET posts");
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  post
+    .save()
+    .then((data) => res.json(data))
+    .catch((err) => res.send({ message: err }));
 });
 
 module.exports = router;
