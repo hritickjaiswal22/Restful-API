14c9b7ac0f22c524973ce44c97d2a907862b7199
Hritick Jaiswal <newbiehritick@gmail.com>
2022-01-04 17:43:54 +0530

    Learned how to retrieve data from database using mongoose

--- routes/posts.js
+++ routes/posts.js
@@ -2,20 +2,27 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("GET posts");
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const post = new Post({
     title: req.body.title,
     description: req.body.description,
   });
 
-  post
-    .save()
-    .then((data) => res.json(data))
-    .catch((err) => res.send({ message: err }));
+  try {
+    const data = await post.save();
+    res.json(data);
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
 module.exports = router;
